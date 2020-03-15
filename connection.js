28a466c2b88a296d1311d6cb8eb8bbe0c890c8eb
CruzCooke <cruzvcooke@gmail.com>
2020-03-16 11:15:02 +1300

    first commit

---
+++ connection.js
@@ -0,0 +1,7 @@
+let dbname = "wednesday_test";
+let username = "root";
+let password = "root";
+
+// exports.atlas = `mongodb+srv://${username}:${password}@yoobee2020-vhp1l.gcp.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+exports.atlas=`mongodb+srv://${username}:${password}@mycluster-vmu7y.mongodb.net/${dbname}?retryWrites=true&w=majority`
---
+++ models/users-model.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+// this will be our data base's data structure
+var UsersSchema = new Schema(
+  {
+    first_name: String,
+    last_name: String,
+    headline: String,
+    subhead: String,
+    company_name: String,
+    address_1: String,
+    address_2: String,
+    po: String,
+    state: String,
+    email: String,
+    image_url: String,
+  },
+  {
+    timestamps: true
+  }
+);
+
+// singular capitalized name for the mongo collection
+module.exports = mongoose.model("User", UsersSchema);
+
+//https://mongoosejs.com/docs/models.html
